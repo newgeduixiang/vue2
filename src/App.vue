d75fc320bdf7181546b5ab085c9b292773ce7e16
zhuzhuzai <17630535830>
2022-07-08 21:49:52 +0800

    项目设置完毕，开发login页面

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-        hello
+<!-- 该根源会替换页面容器，要让其于容器元素保持一致 -->
+    <div id="app">
+        <!-- 根据soter 数据判断首页还是登录页面 -->
+        <!-- 登录页面 -->
+        <login></login>
     </div>
 </template>
 
 <script>
+// 引入组件
+import Login from './views/Login.vue';
 
+export default {
+    // 注册组件
+    components: { Login },
+
+    // 
+}
 
 </script>
 
